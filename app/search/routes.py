85f2a54174a6a978b1b6b0fd6fe6cc4946c5b8b5
dogman0121 <giachetti101@gmail.com>
2025-02-28 15:14:17 +0300

    Fix routes

--- app/search/routes.py
+++ app/search/routes.py
@@ -9,6 +9,7 @@ def search():
             "id": 1,
             "name": "Борьба в прямом эфире",
             "description": "Это чу чука",
+            "poster": "https://cover.imglib.info/uploads/cover/shingeki-no-kyojin/cover/k0HAZuvw2SJw_250x350.jpg",
             "year": 2024,
             "views": 100,
             "created_at": "2024-08-14"
