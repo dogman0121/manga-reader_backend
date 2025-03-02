b1a8ea748d462a703d23f07b4badce7f6ccc7f31
dogman0121 <giachetti101@gmail.com>
2025-03-02 14:28:07 +0300

    Update the plug

--- app/search/routes.py
+++ app/search/routes.py
@@ -7,11 +7,67 @@ def search():
     return jsonify(
         [{
             "id": 1,
+            "status": {
+                "id": 1,
+                "name": "манга"
+            },
+            "type": {
+                "id": 1,
+                "name": "выпускается"
+            },
             "name": "Борьба в прямом эфире",
             "description": "Это чу чука",
             "poster": "https://cover.imglib.info/uploads/cover/shingeki-no-kyojin/cover/k0HAZuvw2SJw_250x350.jpg",
             "year": 2024,
             "views": 100,
-            "created_at": "2024-08-14"
-        }]
+            "created_at": "2024-08-14",
+            "updated_at": "2024-08-14",
+            "genres": [
+                {
+                    "id": 1,
+                    "name": "Драки"
+                },
+                {
+                    "id": 2,
+                    "name": "Седзе"
+                },
+                {
+                    "id": 3,
+                    "name": "Романтика"
+                }
+            ]
+        },
+        {
+            "id": 2,
+            "status": {
+                "id": 1,
+                "name": "манга"
+            },
+            "type": {
+                "id": 1,
+                "name": "выпускается"
+            },
+            "name": "Борьба в прямом эфире",
+            "description": "Это чу чука",
+            "poster": "https://cover.imglib.info/uploads/cover/shingeki-no-kyojin/cover/k0HAZuvw2SJw_250x350.jpg",
+            "year": 2024,
+            "views": 100,
+            "created_at": "2024-08-14",
+            "updated_at": "2024-08-14",
+            "genres": [
+                {
+                    "id": 1,
+                    "name": "Драки"
+                },
+                {
+                    "id": 2,
+                    "name": "Седзе"
+                },
+                {
+                    "id": 3,
+                    "name": "Романтика"
+                }
+            ]
+        },
+        ]
     )
