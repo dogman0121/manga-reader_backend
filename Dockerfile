404fcc6d3b0aa1a69e4506c2ddbfa62479180f47
dogman0121 <giachetti101@gmail.com>
2025-03-11 23:54:30 +0300

    Add migrations and manga tables

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python:3.9-slim-bullseye
 COPY . .
 
 RUN pip install --no-cache-dir -r requirements.txt
-RUN pip install gunicorn pymysql cryptography
+RUN pip install gunicorn
 
 RUN chmod a+x boot.sh
 
--- boot.sh
+++ boot.sh
@@ -1,9 +1,2 @@
-while true; do
-    flask db upgrade
-    if [[ "$?" == "0" ]]; then
-        break
-    fi
-    echo Deploy command failed, retrying in 5 secs...
-    sleep 5
-done
+flask db upgrade
 exec gunicorn -b :8000 --access-logfile - --error-logfile - manage:app
