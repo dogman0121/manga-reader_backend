316c834330ed7c009a2443f0a6eeaa4b20fac53b
dogman0121 <giachetti101@gmail.com>
2025-04-26 22:55:07 +0300

    Add respond function and jwt error handlers

--- app/jwt.py
+++ app/jwt.py
@@ -4,9 +4,9 @@ from app.utils import respond
 jwt = JWTManager()
 
 @jwt.expired_token_loader
-def expired_token_callback():
+def expired_token_callback(jwt_header, jwt_payload):
     return respond(error="unauthorized", detail={"token": "expired"}), 401
 
 @jwt.invalid_token_loader
-def invalid_token_callback():
+def invalid_token_callback(error):
     return respond(error="unauthorized", detail={"token": "invalid"}), 401
